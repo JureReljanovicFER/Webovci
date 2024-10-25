27b8ff472c1117682cb3215579435badde73de03
JureReljanovicFER <jure.reljanovic@fer.hr>
2024-10-25 09:27:47 +0200

    spring boot projekt inicijaliziran

---
+++ IzvorniKod/stanblog/src/test/java/hr/stanblog/stanblog/StanblogApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.stanblog.stanblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
