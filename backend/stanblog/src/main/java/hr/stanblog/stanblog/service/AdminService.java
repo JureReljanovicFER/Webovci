3b2486241b14ab2f2d7c65051f418a9e23087fee
JureReljanovicFER <jure.reljanovic@fer.hr>
2024-10-29 09:47:28 +0100

    dodan AdminService

---
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/service/AdminService.java
@@ -0,0 +1,8 @@
+package hr.stanblog.stanblog.service;
+
+import hr.stanblog.stanblog.dto.AdminDto;
+
+public interface AdminService {
+
+    public String authentificateAdmin(AdminDto adminDto);
+} 
