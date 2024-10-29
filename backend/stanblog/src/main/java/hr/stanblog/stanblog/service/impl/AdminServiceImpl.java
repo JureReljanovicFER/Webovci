6595fedf5143c64127c0cbf762ddf8beaa54382c
JureReljanovicFER <jure.reljanovic@fer.hr>
2024-10-29 09:49:09 +0100

    dodan AdminServiceImpl ali nije implementiran

---
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/service/impl/AdminServiceImpl.java
@@ -0,0 +1,17 @@
+package hr.stanblog.stanblog.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import hr.stanblog.stanblog.dto.AdminDto;
+import hr.stanblog.stanblog.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+    @Override
+    public String authentificateAdmin(AdminDto adminDto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authentificateAdmin'");
+    }
+    
+}
