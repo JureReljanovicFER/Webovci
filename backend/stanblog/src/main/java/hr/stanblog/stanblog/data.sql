e179b74d972f498e2697f6c81818be2026520f94
JureRelja <jurereljanovic99@gmail.com>
2024-11-14 17:04:47 +0100

    promijenjena SQL postavka

--- backend/stanblog/src/main/java/hr/stanblog/stanblog/data.sql
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/data.sql
@@ -1,17 +1,17 @@
 INSERT INTO app_user (id, email, first_name, last_name, user_role)
-VALUES (6, 'jurereljanovic99@gmail.com', 'Jure', 'Reljanovic', 0),
-       (7, 'jurereljanovic987654321@gmail.com', 'Jure', 'Reljanovic', 1),
-       (8, 'asdfasdf@gmail.com', 'Jure', 'Reljanovic', 1),
-       (9, 'ivoivic@gmail.com', 'Ivo', 'Ivic', 1);
+VALUES (1, 'jurereljanovic99@gmail.com', 'Jure', 'Reljanovic', 0),
+       (2, 'jurereljanovic987654321@gmail.com', 'Jure', 'Reljanovic', 1),
+       (3, 'asdfasdf@gmail.com', 'Jure', 'Reljanovic', 1),
+       (4, 'ivoivic@gmail.com', 'Ivo', 'Ivic', 1);
 
 INSERT INTO apartment_building (id, address, city, number_of_individual_apartments, zip_code)
-VALUES (4, 'Odranska 8', 'Zagreb', 23, 10000),
-       (5, 'Splitska 4', 'Split', 5, '21000'),
-       (6, 'Stjepana radica 42', 'Sibenik', 3, 22000);
+VALUES (1, 'Odranska 8', 'Zagreb', 23, 10000),
+       (2, 'Splitska 4', 'Split', 5, '21000'),
+       (3, 'Stjepana radica 42', 'Sibenik', 3, 22000);
 
 INSERT INTO user_apartment_building (id, is_representative, apartment_building_id, user_id)
-VALUES (6, true, 1, 2),
-       (7, false, 2, 3),xw
-       (8, false, 3, 4),
-       (9, false, 1, 5),
-       (10, false, 1, 6);
+VALUES (2, true, 1, 2),
+       (3, false, 2, 3),
+       (1, false, 3, 4),
+       (4, false, 1, 1),
+       (5, false, 1, 6);
