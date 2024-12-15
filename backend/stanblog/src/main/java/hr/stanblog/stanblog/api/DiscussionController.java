0bf75442b1262960eabdbc924271b967f6cbdf65
Lovro Mihaljević <lovro.mihaljevic@fer.hr>
2024-12-15 16:52:37 +0100

    Dodana OpenAPI specifikacija za diskusije

--- backend/stanblog/src/main/java/hr/stanblog/stanblog/api/DiscussionController.java
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/api/DiscussionController.java
@@ -1,6 +1,9 @@
 package hr.stanblog.stanblog.api;
 
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import hr.stanblog.stanblog.model.Discussion;
 import hr.stanblog.stanblog.service.DiscussionService;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "Discussions")
 @RestController
 @RequestMapping("/discussions")
 public class DiscussionController {
@@ -21,6 +25,11 @@ public class DiscussionController {
         this.discussionService = discussionService;
     }
 
+    @ApiOperation(value = "Get all discussions", notes = "Returns a list of all discussions")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list of discussions"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @GetMapping
     public ResponseEntity<List<Discussion>> getAllDiscussions(){
         return new ResponseEntity<List<Discussion>>(discussionService.getAllDiscussions(), HttpStatus.OK);
