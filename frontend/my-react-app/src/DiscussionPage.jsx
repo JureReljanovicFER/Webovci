73afda79bf3553c04a98d23c274e0f59f5648073
CiricFranko1 <franko.ciric@fer.unizg.hr>
2024-12-15 17:32:25 +0100

    added discussion.css, added necessary elements to discussionPage

--- frontend/my-react-app/src/DiscussionPage.jsx
+++ frontend/my-react-app/src/DiscussionPage.jsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
+import "./components/styles/discussion.css";
 
 export default function DiscussionPage() {
-    const params = useParams();
-    console.log(params);
-    return (
-        <>
-            <h1>
-                Building page {params.buildingId} Disscussion {params.discussionId}
-            </h1>
-        </>
-    );
+  const params = useParams();
+  console.log(params);
+  return (
+    <>
+      <div class="discussion_container"></div>
+      <div class="discussion_footer">
+        <button class="discussion_btn">Dodaj komentar</button>
+      </div>
+    </>
+  );
 }
