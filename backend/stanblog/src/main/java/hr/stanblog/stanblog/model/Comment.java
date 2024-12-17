d4a8795ca1f9399c942054b6c0a87703e5ee7952
Lovro Mihaljević <lovro.mihaljevic@fer.hr>
2024-12-17 15:59:23 +0100

    Nadopunjena klasa za komentare

--- backend/stanblog/src/main/java/hr/stanblog/stanblog/model/Comment.java
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/model/Comment.java
@@ -1,12 +1,25 @@
 package hr.stanblog.stanblog.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table
 public class Comment {
     @Id
      Long id;
+
+    @ManyToOne
+    private Discussion discussion;
+
+    @ManyToOne
+    private AppUser author;
+
+    @ManyToOne
+    private Comment parentComment;
+
+    private String content;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
 }
