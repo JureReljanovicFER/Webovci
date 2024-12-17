5770055052d84405717f16635a8235b11eccdc5f
JureRelja <jurereljanovic99@gmail.com>
2024-12-17 16:48:17 +0100

    male promjene

--- backend/stanblog/src/main/java/hr/stanblog/stanblog/api/CommentController.java
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/api/CommentController.java
@@ -3,10 +3,12 @@ package hr.stanblog.stanblog.api;
 import hr.stanblog.stanblog.model.Comment;
 import hr.stanblog.stanblog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:5173", "https://jazzy-madeleine-64561a.netlify.app", "https://webovci-1.onrender.com"})
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
@@ -18,8 +20,8 @@ public class CommentController {
         return commentService.addCommentToDiscussion(discussionId, userId, content);
     }
 
-    @PostMapping
-    public Comment addReplyToComment(Long parentCommentId, Long userId, String content){
-        return commentService.addReplyToComment(parentCommentId, userId, content);
-    }
+//    @PostMapping
+//    public Comment addReplyToComment(Long parentCommentId, Long userId, String content){
+//        return commentService.addReplyToComment(parentCommentId, userId, content);
+//    }
 }
