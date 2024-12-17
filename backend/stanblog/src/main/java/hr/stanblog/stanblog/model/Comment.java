3d4acfac596e17f7bfa00cba820ad58ca994efa7
Lovro Mihaljević <lovro.mihaljevic@fer.hr>
2024-12-17 16:35:19 +0100

    Implementirana funkcionalnost komentara i odgovora

--- backend/stanblog/src/main/java/hr/stanblog/stanblog/model/Comment.java
+++ backend/stanblog/src/main/java/hr/stanblog/stanblog/model/Comment.java
@@ -22,4 +22,44 @@ public class Comment {
     private String content;
 
     private final LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Discussion getDiscussion() {
+        return discussion;
+    }
+
+    public void setDiscussion(Discussion discussion) {
+        this.discussion = discussion;
+    }
+
+    public AppUser getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AppUser author) {
+        this.author = author;
+    }
+
+    public Comment getParentComment() {
+        return parentComment;
+    }
+
+    public void setParentComment(Comment parentComment) {
+        this.parentComment = parentComment;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
